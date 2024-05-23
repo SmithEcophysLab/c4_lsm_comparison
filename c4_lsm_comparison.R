d77888deaebe9bb46ab444cb3bf7d83bd004d2b1
Nick <nick.gregory.smith@gmail.com>
2024-05-23 16:40:41 -0600

    updated equations and added figures still need to fix bethy temperature function

--- c4_lsm_comparison.R
+++ c4_lsm_comparison.R
@@ -1,6 +1,10 @@
 # c4_lsm_comparison.R
 ## script to compare c4 leaf photosynthesis responses in lsms
 ## all models use some version of collatz
+
+## load libraries
+library(tidyverse)
+
 ## steps:
 ### 1. temperature response function for each lsm
 ### 2. collatz model function
@@ -133,22 +137,109 @@ collatz1992_func <- function(tleaf = 298.15,
 
 ### 3. lsm-specific simulations in response to light, temperature, CO2
 #### light
-bethy_light <- collatz1992_func(par = seq(50, 2000, 50), phi = 0.04, model = 'bethy')
-clm_light <- collatz1992_func(par = seq(50, 2000, 50), phi = 0.05, model = 'clm')
-jules_light <- collatz1992_func(par = seq(50, 2000, 50), phi = 0.04, model = 'jules')
-pmodel_light <- collatz1992_func(par = seq(50, 2000, 50), model = 'pmodel')
+bethy_light <- collatz1992_func(par = seq(20, 2000, 20), phi = 0.04, model = 'bethy')
+clm_light <- collatz1992_func(par = seq(20, 2000, 20), phi = 0.05, model = 'clm')
+jules_light <- collatz1992_func(par = seq(20, 2000, 20), phi = 0.04, model = 'jules')
+pmodel_light <- collatz1992_func(par = seq(20, 2000, 20), model = 'pmodel')
 
 #### temperature
-bethy_temperature <- collatz1992_func(tleaf = seq(278.15, 313.15, 5), 
+bethy_temperature <- collatz1992_func(tleaf = seq(278.15, 323.15, 5), 
                                 phi = 0.04, model = 'bethy')
-clm_temperature <- collatz1992_func(tleaf = seq(278.15, 313.15, 5), 
+clm_temperature <- collatz1992_func(tleaf = seq(278.15, 323.15, 5), 
                               phi = 0.05, model = 'clm', use_ftemp = TRUE)
-jules_temperature <- collatz1992_func(tleaf = seq(278.15, 313.15, 5), 
+jules_temperature <- collatz1992_func(tleaf = seq(278.15, 323.15, 5), 
                                 phi = 0.04, model = 'jules', use_ftemp = TRUE)
-pmodel_temperature <- collatz1992_func(tleaf = seq(278.15, 313.15, 5), 
+pmodel_temperature <- collatz1992_func(tleaf = seq(278.15, 323.15, 5), 
                                  model = 'pmodel')
 
+#### co2
+bethy_co2 <- collatz1992_func(ca = seq(20, 1000, 20), 
+                                      phi = 0.04, model = 'bethy')
+clm_co2 <- collatz1992_func(ca = seq(20, 1000, 20), 
+                                    phi = 0.05, model = 'clm', use_ftemp = TRUE)
+jules_co2 <- collatz1992_func(ca = seq(20, 1000, 20), 
+                                      phi = 0.04, model = 'jules', use_ftemp = TRUE)
+pmodel_co2 <- collatz1992_func(ca = seq(20, 1000, 20), 
+                                       model = 'pmodel')
+
 ### 4. make figures
+light_plot <- ggplot(data = bethy_light, aes(x = par, y = assimilation)) +
+  theme(legend.position = "right", 
+        legend.text=element_text(size=15),
+        legend.title=element_text(size=15),
+        axis.title.y=element_text(size=rel(2.5), colour = 'black'),
+        axis.title.x=element_text(size=rel(2.5), colour = 'black'),
+        axis.text.x=element_text(size=rel(2), colour = 'black'),
+        axis.text.y=element_text(size=rel(2), colour = 'black'),
+        panel.background = element_rect(fill = 'white', colour = 'black'),
+        panel.grid.major = element_line(colour = "grey")) +
+  geom_line(lty = 1, linewidth = 2, color = 'orange', 
+            alpha = 0.5, position=position_jitter(w=10, h=0)) +
+  geom_line(lty = 1, linewidth = 2, color = 'blue', 
+            alpha = 0.5, position=position_jitter(w=10, h=0),
+            data = clm_light) +
+  geom_line(lty = 1, linewidth = 2, color = 'purple', 
+            alpha = 0.5, position=position_jitter(w=10, h=0),
+            data = jules_light) +
+  geom_line(lty = 1, linewidth = 2, color = 'red', 
+            alpha = 0.5, position=position_jitter(w=10, h=0),
+            data = pmodel_light) +
+  ylim(c(0, 10)) +
+  xlim(c(0, 2000)) +
+  ylab(expression('Photosynthetic C assimilation (µmol m' ^ '2' * ' s' ^ '-1' * ')')) +
+  xlab(expression('Photosynthetically active radiation (µmol m' ^ '2' * ' s' ^ '-1' * ')'))
+
+temperature_plot <- ggplot(data = bethy_temperature, aes(x = tleaf, y = assimilation)) +
+  theme(legend.position = "right", 
+        legend.text=element_text(size=15),
+        legend.title=element_text(size=15),
+        axis.title.y=element_text(size=rel(2.5), colour = 'black'),
+        axis.title.x=element_text(size=rel(2.5), colour = 'black'),
+        axis.text.x=element_text(size=rel(2), colour = 'black'),
+        axis.text.y=element_text(size=rel(2), colour = 'black'),
+        panel.background = element_rect(fill = 'white', colour = 'black'),
+        panel.grid.major = element_line(colour = "grey")) +
+  geom_line(lty = 1, linewidth = 2, color = 'orange', 
+            alpha = 0.5, position=position_jitter(w=0, h=0)) +
+  geom_line(lty = 1, linewidth = 2, color = 'blue', 
+            alpha = 0.5, position=position_jitter(w=0, h=0),
+            data = clm_temperature) +
+  geom_line(lty = 1, linewidth = 2, color = 'purple', 
+            alpha = 0.5, position=position_jitter(w=0, h=0),
+            data = jules_temperature) +
+  geom_line(lty = 1, linewidth = 2, color = 'red', 
+            alpha = 0.5, position=position_jitter(w=0, h=0),
+            data = pmodel_temperature) +
+  ylim(c(0, 25)) +
+  xlim(c(275, 320)) +
+  ylab(expression('Photosynthetic C assimilation (µmol m' ^ '2' * ' s' ^ '-1' * ')')) +
+  xlab(expression('Leaf temperature (°C)'))
+
+co2_plot <- ggplot(data = bethy_co2, aes(x = ca, y = assimilation)) +
+  theme(legend.position = "right", 
+        legend.text=element_text(size=15),
+        legend.title=element_text(size=15),
+        axis.title.y=element_text(size=rel(2.5), colour = 'black'),
+        axis.title.x=element_text(size=rel(2.5), colour = 'black'),
+        axis.text.x=element_text(size=rel(2), colour = 'black'),
+        axis.text.y=element_text(size=rel(2), colour = 'black'),
+        panel.background = element_rect(fill = 'white', colour = 'black'),
+        panel.grid.major = element_line(colour = "grey")) +
+  geom_line(lty = 1, linewidth = 2, color = 'orange', 
+            alpha = 0.5, position=position_jitter(w=0, h=0)) +
+  geom_line(lty = 1, linewidth = 2, color = 'blue', 
+            alpha = 0.5, position=position_jitter(w=0, h=0),
+            data = clm_co2) +
+  geom_line(lty = 1, linewidth = 2, color = 'purple', 
+            alpha = 0.5, position=position_jitter(w=0, h=0),
+            data = jules_co2) +
+  geom_line(lty = 1, linewidth = 2, color = 'red', 
+            alpha = 0.5, position=position_jitter(w=0, h=0),
+            data = pmodel_co2) +
+  ylim(c(0, 10)) +
+  xlim(c(0, 1000)) +
+  ylab(expression('Photosynthetic C assimilation (µmol m' ^ '2' * ' s' ^ '-1' * ')')) +
+  xlab(expression('Atmospheric CO'[2] * ' (ppm)'))
 
 
 
